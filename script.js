3bd31dab9c95b8ac32016f6aea42760e036661ce
Ivan Shpynda <ivanshpynda@gmail.com>
2023-01-04 23:12:16 +0200

    refactor code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 import anime from './animejs/lib/anime.es.js';
+
 const startBtn = document.querySelector('.start-btn');
 const settingSection = document.querySelector('.game-settings');
 const gameFieldWrapper = document.querySelector('.game-field-wrapper');
@@ -10,9 +11,28 @@ const replayBtn = document.querySelector('.replay-btn');
 startBtn.addEventListener('click', initializeGame);
 replayBtn.addEventListener('click', replayGame);
 
-/**
- * Class to generate activity field
- */
+function initializeGame() {
+  const gameParameters = {
+    columns: document.getElementById('columns').value,
+    rows: document.getElementById('rows').value,
+    width: document.getElementById('width').value,
+    height: document.getElementById('height').value,
+    timeLimit: document.getElementById('time-limit').value,
+    color: document.getElementById('theme').value,
+  };
+
+  const game = new MatchGrid(gameParameters);
+  game.startGame();
+}
+
+function replayGame() {
+  settingSection.style.display = 'block';
+  gameFieldWrapper.style.display = 'none';
+  cardsWrapper.textContent = '';
+  timeIndicator.style.color = 'inherit';
+  replayBtn.style.display = 'none';
+};
+
 class MatchGrid {
   constructor({columns, rows, width, height, timeLimit, color}) {
     this.columns = columns;
@@ -22,9 +42,37 @@ class MatchGrid {
     this.timeLimit = timeLimit;
     this.color = color;
     this.values = MatchGrid.generateCardValues(columns * rows);
+    this.chosenCardsIds = [];
+    this.chosenCardsValues = [];
+    this.uniqueValuesNumber = this.values.length / 2;
+    this.counter = 0;
+    this.timer = null;
+    this.boundFlipCard = this.flipCard.bind(this);
   }
 
-  // Method to create cards
+  startGame() {
+    settingSection.style.display = 'none';
+    gameFieldWrapper.style.display = 'block';
+    const cardsContainer = this.createCards();
+
+    cardsContainer.childNodes.forEach((card) => {
+      card.addEventListener('click', this.boundFlipCard);
+    });
+
+    cardsWrapper.appendChild(cardsContainer);
+
+    anime({
+      targets: '.game-card',
+      translateY: -60,
+      delay: anime.stagger(100, {from: 'center'}),
+      direction: 'reverse',
+    });
+
+    this.timer = this.setTimer();
+    cardsContainer.addEventListener('mouseleave', this.timer.pause.bind(this));
+    cardsContainer.addEventListener('mouseenter', this.timer.resume.bind(this));
+  }
+
   createCards() {
     const cardsContainer = document.createElement('div');
     cardsContainer.classList.add('game-cards');
@@ -44,85 +92,52 @@ class MatchGrid {
     return cardsContainer;
   }
 
-  // method to create values for cards
-  static generateCardValues(len) {
-    const values = [];
-
-    while (values.length < (len / 2)) {
-      const value = Math.floor(Math.random() * len) + 1;
-      if (values.indexOf(value) === -1) values.push(value);
-    }
-
-    values.push(...values);
-    values.sort(() => 0.5 - Math.random());
-
-    return values;
-  }
-}
-
-/**
- * Function to initialize a game
- */
-function initializeGame() {
-  const gameParameters = {
-    columns: document.getElementById('columns').value,
-    rows: document.getElementById('rows').value,
-    width: document.getElementById('width').value,
-    height: document.getElementById('height').value,
-    timeLimit: document.getElementById('time-limit').value,
-    color: document.getElementById('theme').value,
-  };
-
-  settingSection.style.display = 'none';
-  gameFieldWrapper.style.display = 'block';
-
-  const gameField = new MatchGrid(gameParameters);
-  const cardsContainer = gameField.createCards();
-  const uniqueValuesNumber = gameField.values.length / 2;
-  let counter = 0;
-
-  // add event to each card
-  cardsContainer.childNodes.forEach((card) => {
-    card.addEventListener('click', flipCard);
-  });
-
-  // put cards container into dom
-  cardsWrapper.appendChild(cardsContainer);
-
-  // animation for the cards
-  anime({
-    targets: '.game-card',
-    translateY: -60,
-    delay: anime.stagger(100, {from: 'center'}),
-    direction: 'reverse',
-  });
-
-  // setting timer for the game
-  const timer = setTimer();
-  cardsContainer.addEventListener('mouseleave', timer.pause);
-  cardsContainer.addEventListener('mouseenter', timer.resume);
-
-  // ids and values of selected cards
-  const chosenCardsIds = [];
-  const chosenCardsValues = [];
-
-  // function to handle card selection
-  function flipCard(e) {
+  flipCard(e) {
     const flippedCard = e.target;
     const id = flippedCard.getAttribute('data-id');
     const value = flippedCard.getAttribute('data-value');
     flippedCard.textContent = value;
 
-    chosenCardsIds.push(id);
-    chosenCardsValues.push(value);
-    if (chosenCardsIds.length === 2) {
-      checkForMatch();
+    this.chosenCardsIds.push(id);
+    this.chosenCardsValues.push(value);
+    if (this.chosenCardsIds.length === 2) {
+      this.checkForMatch();
     }
-  };
+  }
 
-  // timer function
-  function setTimer() {
-    let time = +gameParameters.timeLimit;
+  checkForMatch() {
+    const cards = document.querySelectorAll('.game-card')
+    const firstSelected = this.chosenCardsIds[0];
+    const secondSelected = this.chosenCardsIds[1];
+
+    if (firstSelected === secondSelected) {
+      cards[firstSelected].textContent = '';
+    } else if (this.chosenCardsValues[0] === this.chosenCardsValues[1]) {
+      cards[firstSelected].removeEventListener('click', this.boundFlipCard);
+      cards[secondSelected].removeEventListener('click', this.boundFlipCard);
+
+      setTimeout(() => {
+        cards[firstSelected].textContent = '';
+        cards[secondSelected].textContent = '';
+        cards[firstSelected].style.backgroundColor = '#fefefe';
+        cards[secondSelected].style.backgroundColor = '#fefefe';
+        this.counter++;
+        if (this.counter === this.uniqueValuesNumber) this.gameWon();
+      }, 500);
+
+    } else {
+      setTimeout(() => {
+        cards[firstSelected].textContent = '';
+        cards[secondSelected].textContent = '';
+      }, 500);
+    }
+
+      this.chosenCardsIds.splice(0, 2);
+      this.chosenCardsValues.splice(0, 2);
+  }
+
+  setTimer() {
+    let time = +this.timeLimit;
     let paused = true;
 
     const timerCountDown = setInterval(() => {
@@ -133,9 +148,10 @@ function initializeGame() {
         timeIndicator.textContent = '🔴 Time Limit Exceeded';
         replayBtn.style.display = 'block';
         const cards = document.querySelectorAll('.game-card');
+        console.log(cards);
         cards.forEach(card => {
           card.textContent = '';
-          card.removeEventListener('click', flipCard);
+          card.removeEventListener('click', this.boundFlipCard);
         })
       }
     }, 1000);
@@ -151,53 +167,23 @@ function initializeGame() {
     };
   }
 
-  // function to check for match
-  function checkForMatch() {
-    const cards = document.querySelectorAll('.game-card')
-    const firstSelected = chosenCardsIds[0];
-    const secondSelected = chosenCardsIds[1];
+  gameWon() {
+  this.timer.stop();
+  replayBtn.style.display = 'block';
+  timeIndicator.textContent = 'You have found all matches! 🥳';
+  }
 
-    if (firstSelected === secondSelected) {
-      cards[firstSelected].textContent = '';
-    } else if (chosenCardsValues[0] === chosenCardsValues[1]) {
-      cards[firstSelected].removeEventListener('click', flipCard);
-      cards[secondSelected].removeEventListener('click', flipCard);
+  static generateCardValues(len) {
+    const values = [];
 
-      setTimeout(() => {
-        cards[firstSelected].textContent = '';
-        cards[secondSelected].textContent = '';
-        cards[firstSelected].style.backgroundColor = '#fefefe';
-        cards[secondSelected].style.backgroundColor = '#fefefe';
-        counter++;
-        if (counter === uniqueValuesNumber) gameWon();
-      }, 500);
-
-    } else {
-      setTimeout(() => {
-        cards[firstSelected].textContent = '';
-        cards[secondSelected].textContent = '';
-      }, 500);
+    while (values.length < (len / 2)) {
+      const value = Math.floor(Math.random() * len) + 1;
+      if (values.indexOf(value) === -1) values.push(value);
     }
 
-      chosenCardsIds.splice(0, 2);
-      chosenCardsValues.splice(0, 2);
-  };
+    values.push(...values);
+    values.sort(() => 0.5 - Math.random());
 
-  // function to run if all matches are found
-  function gameWon() {
-    timer.stop();
-    replayBtn.style.display = 'block';
-    timeIndicator.textContent = 'You have found all matches! 🥳';
+    return values;
   }
 }
-
-/**
- * Function to replay the game
- */
-function replayGame() {
-  settingSection.style.display = 'block';
-  gameFieldWrapper.style.display = 'none';
-  cardsWrapper.textContent = '';
-  timeIndicator.style.color = 'inherit';
-  replayBtn.style.display = 'none';
-};
